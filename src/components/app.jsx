553dbf3a671521695ffcc8f7f97e4b69c9961410
hankfrank <henryfrank1@yahoo.com>
2019-12-10 14:24:33 -0800

    wires up index.js and App.jsx to instantiate main react component

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import { render } from 'react-dom';
+
+function App () {
+    return (
+        <h1>Testing this hot dirty mother!</h1>
+    )
+};
+
+export default App;
