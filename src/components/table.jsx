56ecb47df505f0f4ee4d0a6d8a2e28a22d410e4e
hankfrank <henryfrank1@yahoo.com>
2020-12-22 12:03:16 -0800

    working table on Mac and windows

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -3,17 +3,17 @@ import React, { useState, useEffect } from 'react';
 import {Column, Table} from 'react-virtualized';
 import 'react-virtualized/styles.css';
 import { DataGrid, Button, withTheme } from '@material-ui/core';
+import { grey } from '@material-ui/core/colors';
 
 const MyTable = (props) => {
     const [selected, setSelected] = useState([]);
     const [indexes, setIndexes] = useState([]);
     const [myTable, setMyTable] = useState([]);
-    const [lastClicked, setLastClicked] = useState(0);
-
+    const [lastClicked, setLastClicked] = useState();
     const [list, setList] = useState([]);
 
-    //create placeholder table data 
     useEffect(() => {
+        //create placeholder table data 
         let tempList = list;
         for (let i = 0; i < 10000; i++) {
             tempList.push({
@@ -24,18 +24,23 @@ const MyTable = (props) => {
         }
         setList(tempList);
     }, []);
-
+    
     const rowStyles = (row) => {
-        if ( indexes.includes(row.index) )
-        return {
-            backgroundColor: "#2d87b3", 
-            color: "white"
+        if ( indexes.includes(row.index) ) {
+            return {
+                backgroundColor: "#2d87b3", 
+                color: "white",
+                borderRight: '1px solid black'
+            } 
+        } else if (row.index === -1) {
+            return {
+                backgroundColor: "#808080", 
+                color: "white"
+            }
         }
     };
 
     useEffect(() => {
-        console.log('creating table');
-        
         setMyTable(<Table 
             width={ 600 }
             height={ 500 }
@@ -45,6 +50,7 @@ const MyTable = (props) => {
             rowGetter={ ({index}) => list[index] }
             onRowClick={ rowClickEvent }
             rowStyle={ rowStyles }
+            style={ {width: 'fit-content', border: "2px solid black"} }
         >
             <Column 
                 label="Name"
@@ -74,6 +80,7 @@ const MyTable = (props) => {
     }
 
     const filterSelectedForUnique = (array) => {
+        // This uses an object property to check for duplicates, if 'name' is not a unique object property a different unique object prop will need to be used
         let flags = {};
         let final = array.filter((each) => {
             if (flags[each.name]) {
@@ -90,15 +97,19 @@ const MyTable = (props) => {
         const indexClicked = event.index;
         const shift = event.event.shiftKey;
         const cmnd = event.event.metaKey;
+        const alt = event.event.altKey;
+        const ctrl = event.event.ctrlKey;
+        //the arrays here need to be copied this way so that the depencendy array of the useEffect above recognizes them as a state change, otherwise the table will never re-render. 
         let tempIndexes = [...indexes];
         let tempSelected = [...selected];
         setLastClicked(indexClicked);
 
         if (shift) {
-            //need to check if clicked and last clicked are both inside of a selected range and add ot conditional for range deselsect
+            //need to check if clicked and last clicked are both inside of a selected range and add to conditional for range deselsect
             if (tempIndexes.includes(indexClicked) && tempIndexes.includes(lastClicked) && indexClicked > lastClicked) {
                 tempIndexes.splice(lastClicked, Math.abs(indexClicked - lastClicked) + 1);
                 tempSelected.splice(lastClicked, Math.abs(indexClicked - lastClicked) + 1);
+                //these filter for unique are not necessary for each and every one of these it was just easier to add it to all of them
                 setIndexes(filterIndexesForUnique(tempIndexes));
                 setSelected(filterSelectedForUnique(tempSelected));
             } else if (indexClicked > lastClicked) {
@@ -115,8 +126,14 @@ const MyTable = (props) => {
                 }
                 setIndexes(filterIndexesForUnique(tempIndexes));
                 setSelected(filterSelectedForUnique(tempSelected));
-            }        
-        } else if (cmnd) {
+            } else {
+                tempIndexes.push(indexClicked);
+                tempSelected.push(list[indexClicked]);
+                setIndexes(filterIndexesForUnique(tempIndexes));                       
+                setSelected(filterSelectedForUnique(tempSelected));
+            }     
+        } else if (cmnd || ctrl) {
+            //This works on Mac, the ctrl registers as a right click and so the click event of the ctrl never registers in teh browser as the OS picks it up. 
             if (tempIndexes.includes(indexClicked)) {
                 for (let i = 0; i < tempIndexes.length; i ++) {
                     if (indexClicked === tempIndexes[i]) {
@@ -164,7 +181,7 @@ const MyTable = (props) => {
         <>
             <Button onClick={ testButton } variant="contained">TestButton</Button>
             { myTable }
-            <Button onClick={ submitTable }  variant="contained"> Submit </Button>
+            <Button onClick={ submitTable }  variant="contained" > Submit </Button>
         </>
     );
 };
